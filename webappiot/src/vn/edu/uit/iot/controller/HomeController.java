925382d72e0ea5646f483786e8c3c85a4090918f
hainguyen <hainguyen@HP>
2016-10-26 00:29:46 +0700

    update resource file

--- webappiot/src/vn/edu/uit/iot/controller/HomeController.java
+++ webappiot/src/vn/edu/uit/iot/controller/HomeController.java
@@ -1,5 +1,16 @@
 package vn.edu.uit.iot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class HomeController {
+	@RequestMapping(value="/" , method= RequestMethod.GET)
+	public ModelAndView home(ModelAndView mModelAndView){
+		mModelAndView = new ModelAndView("index");
+		return mModelAndView;
+	}
 
 }
